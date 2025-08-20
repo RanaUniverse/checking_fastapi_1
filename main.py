14a8803a31bf47b66886bd8dd854e0a458625f5d
RanaUniverse <RanaUniverse321@gmail.com>
2025-08-20 16:11:46 +0530

    make a fun for the get so that it will easy to separate the logics

--- main.py
+++ main.py
@@ -9,18 +9,25 @@ base_url = "http://localhost:8000"
 files_endpoint = "/files/abc.txt"
 
 url = base_url + files_endpoint
+url = "http://localhost:8000/users/12/items/98"
 
-response = httpx.get(url)
 
-status_code = response.status_code
+def check_get_1():
+    response = httpx.get(url)
 
-if status_code == 200:
-    print("Good Connection Established with 200")
+    status_code = response.status_code
 
-    resp_dict = json.loads(response.text)
-    print(resp_dict)
+    if status_code == 200:
+        print("Good Connection Established with 200")
 
-else:
-    print("Not Good Result")
-    print("Status Code:", response.status_code)
+        resp_dict = json.loads(response.text)
+        print(resp_dict)
 
+    else:
+        print("Not Good Result")
+        print("Status Code:", response.status_code)
+        print(response.json())
+
+
+if __name__ == "__main__":
+    check_get_1()
