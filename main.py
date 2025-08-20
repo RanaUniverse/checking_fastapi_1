c5209fb6d375415c19f87a46eeb0f5ac591f760b
RanaUniverse <RanaUniverse321@gmail.com>
2025-08-20 17:02:25 +0530

    a post checking

--- main.py
+++ main.py
@@ -6,13 +6,11 @@ import httpx
 
 base_url = "http://localhost:8000"
 
-files_endpoint = "/files/abc.txt"
-
-url = base_url + files_endpoint
-url = "http://localhost:8000/users/12/items/98"
-
 
 def check_get_1():
+    files_endpoint = "/files/abc.txt"
+
+    url = base_url + files_endpoint
     response = httpx.get(url)
 
     status_code = response.status_code
@@ -29,5 +27,38 @@ def check_get_1():
         print(response.json())
 
 
+def check_post_1():
+
+    final_endpoint = "/items/"
+
+    url = base_url + final_endpoint
+
+    payload: dict[str, str | int] = {
+        "name": "Rana Universe Book",
+        "price": 250,
+        "description": "This is a good book written for our universe.",
+        "tax": 20,
+    }
+
+    params = {"tax": 15}
+
+    response = httpx.post(url, json=payload, params=params)
+
+    status_code = response.status_code
+
+    if status_code == 200:
+        print("✅ POST request success (200 OK)")
+        resp_dict = json.loads(response.text)
+        print(resp_dict)
+    else:
+        print("❌ POST request failed")
+        print("Status Code:", response.status_code)
+        try:
+            print(response.json())
+        except Exception:
+            print(response.text)
+
+
 if __name__ == "__main__":
-    check_get_1()
+    # check_get_1()
+    check_post_1()
